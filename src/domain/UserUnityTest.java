7826868c6c91cbf010aa109e5915909afd0af462
Leonardo Rodrigues <lmrodrigues@inf.ufrgs.br>
2016-12-04 00:54:48 -0200

    Some JUnit tests class added

---
+++ src/domain/UserUnityTest.java
@@ -0,0 +1,208 @@
+/**
+ * 
+ */
+package domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author lmrodrigues
+ *
+ */
+public class UserUnityTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * Test method for {@link domain.User#User()}.
+     */
+    @Test
+    public final void testUser() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.User#User(java.lang.String, java.lang.String, java.lang.String, java.lang.String)}.
+     */
+    @Test
+    public final void testUserStringStringStringString() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.User#getUsername()}.
+     */
+    @Test
+    public final void testGetUsername() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.User#setUsername(java.lang.String)}.
+     */
+    @Test
+    public final void testSetUsername() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.User#getPassword()}.
+     */
+    @Test
+    public final void testGetPassword() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.User#setPassword(java.lang.String)}.
+     */
+    @Test
+    public final void testSetPassword() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.User#getEmail()}.
+     */
+    @Test
+    public final void testGetEmail() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.User#setEmail(java.lang.String)}.
+     */
+    @Test
+    public final void testSetEmail() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.User#getName()}.
+     */
+    @Test
+    public final void testGetName() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.User#setName(java.lang.String)}.
+     */
+    @Test
+    public final void testSetName() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.User#isBlock()}.
+     */
+    @Test
+    public final void testIsBlock() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.User#blockUser()}.
+     */
+    @Test
+    public final void testBlockUser() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.User#unblockUser()}.
+     */
+    @Test
+    public final void testUnblockUser() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.User#getBlockStatus()}.
+     */
+    @Test
+    public final void testGetBlockStatus() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.User#setBlockStatus(java.lang.Boolean)}.
+     */
+    @Test
+    public final void testSetBlockStatus() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.User#getUserPermission()}.
+     */
+    @Test
+    public final void testGetUserPermission() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.User#setUserPermission(domain.Permission)}.
+     */
+    @Test
+    public final void testSetUserPermission() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.User#getUserQuestions()}.
+     */
+    @Test
+    public final void testGetUserQuestions() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.User#setUserQuestions(java.util.List)}.
+     */
+    @Test
+    public final void testSetUserQuestions() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.User#getUserAnswers()}.
+     */
+    @Test
+    public final void testGetUserAnswers() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.User#setUserAnswers(java.util.List)}.
+     */
+    @Test
+    public final void testSetUserAnswers() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.User#getUserComments()}.
+     */
+    @Test
+    public final void testGetUserComments() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.User#setUserComments(java.util.List)}.
+     */
+    @Test
+    public final void testSetUserComments() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
---
+++ src/serviceApi/ServiceAPITest.java
@@ -0,0 +1,192 @@
+/**
+ * 
+ */
+package serviceApi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author lmrodrigues
+ *
+ */
+public class ServiceAPITest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * Test method for {@link serviceApi.ServiceAPI#newAccount(java.lang.String, java.lang.String, java.lang.String, java.lang.String, domain.Permission)}.
+     */
+    @Test
+    public final void testNewAccount() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link serviceApi.ServiceAPI#login(java.lang.String, java.lang.String)}.
+     */
+    @Test
+    public final void testLogin() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link serviceApi.ServiceAPI#logout()}.
+     */
+    @Test
+    public final void testLogout() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link serviceApi.ServiceAPI#blockUser(java.lang.String)}.
+     */
+    @Test
+    public final void testBlockUser() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link serviceApi.ServiceAPI#unblockUser(java.lang.String)}.
+     */
+    @Test
+    public final void testUnblockUser() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link serviceApi.ServiceAPI#changeUserPermission(java.lang.String, domain.Permission)}.
+     */
+    @Test
+    public final void testChangeUserPermission() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link serviceApi.ServiceAPI#changeUserPassword(java.lang.String)}.
+     */
+    @Test
+    public final void testChangeUserPassword() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link serviceApi.ServiceAPI#newQuestion(java.lang.String, java.util.List, java.lang.String)}.
+     */
+    @Test
+    public final void testNewQuestion() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link serviceApi.ServiceAPI#newAnswer(java.lang.String, java.lang.Integer)}.
+     */
+    @Test
+    public final void testNewAnswer() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link serviceApi.ServiceAPI#newComment(java.lang.String, domain.AbstractContent)}.
+     */
+    @Test
+    public final void testNewComment() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link serviceApi.ServiceAPI#editContent(domain.AbstractContent)}.
+     */
+    @Test
+    public final void testEditContent() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link serviceApi.ServiceAPI#deleteContent(domain.AbstractContent)}.
+     */
+    @Test
+    public final void testDeleteContent() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link serviceApi.ServiceAPI#selectQuestion(java.lang.Integer)}.
+     */
+    @Test
+    public final void testSelectQuestion() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link serviceApi.ServiceAPI#searchQuestion(java.lang.String)}.
+     */
+    @Test
+    public final void testSearchQuestion() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link serviceApi.ServiceAPI#listAllQuestions()}.
+     */
+    @Test
+    public final void testListAllQuestions() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link serviceApi.ServiceAPI#bestAnswer(java.lang.Integer, java.lang.Integer)}.
+     */
+    @Test
+    public final void testBestAnswer() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link serviceApi.ServiceAPI#closeQuestion(java.lang.Integer)}.
+     */
+    @Test
+    public final void testCloseQuestion() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link serviceApi.ServiceAPI#openQuestion(java.lang.Integer)}.
+     */
+    @Test
+    public final void testOpenQuestion() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link serviceApi.ServiceAPI#upVoteAnswer(java.lang.Integer)}.
+     */
+    @Test
+    public final void testUpVoteAnswer() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link serviceApi.ServiceAPI#downVoteAnswer(java.lang.Integer)}.
+     */
+    @Test
+    public final void testDownVoteAnswer() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link serviceApi.ServiceAPI#getLoggedUser()}.
+     */
+    @Test
+    public final void testGetLoggedUser() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
