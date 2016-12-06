1430bbdb4972d18626200cf4d0e759b3503e84fe
Leonardo Rodrigues <lmrodrigues@inf.ufrgs.br>
2016-12-06 01:28:14 -0200

    Contents Tests Classes Added

---
+++ src/domain/QuestionTest.java
@@ -0,0 +1,176 @@
+/**
+ * 
+ */
+package domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author lmrodrigues
+ *
+ */
+public class QuestionTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * Test method for {@link domain.Question#Question(java.lang.Integer, domain.User, java.lang.String, java.lang.String, java.util.List)}.
+     */
+    @Test
+    public final void testQuestion() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.Question#getTitle()}.
+     */
+    @Test
+    public final void testGetTitle() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.Question#setTitle(java.lang.String)}.
+     */
+    @Test
+    public final void testSetTitle() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.Question#getTags()}.
+     */
+    @Test
+    public final void testGetTags() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.Question#setTags(java.util.List)}.
+     */
+    @Test
+    public final void testSetTags() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.Question#getAnswers()}.
+     */
+    @Test
+    public final void testGetAnswers() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.Question#setAnswers(java.util.List)}.
+     */
+    @Test
+    public final void testSetAnswers() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.Question#addAnswer(domain.Answer)}.
+     */
+    @Test
+    public final void testAddAnswer() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.Question#delAnswer(domain.Answer)}.
+     */
+    @Test
+    public final void testDelAnswer() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.Question#getComments()}.
+     */
+    @Test
+    public final void testGetComments() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.Question#setComments(java.util.List)}.
+     */
+    @Test
+    public final void testSetComments() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.Question#addComment(domain.Comment)}.
+     */
+    @Test
+    public final void testAddComment() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.Question#delComment(domain.Comment)}.
+     */
+    @Test
+    public final void testDelComment() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.Question#getBestAnswer()}.
+     */
+    @Test
+    public final void testGetBestAnswer() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.Question#setBestAnswer(domain.Answer)}.
+     */
+    @Test
+    public final void testSetBestAnswer() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.Question#getStatus()}.
+     */
+    @Test
+    public final void testGetStatus() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.Question#setStatus(domain.Status)}.
+     */
+    @Test
+    public final void testSetStatus() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.Question#openQuestion()}.
+     */
+    @Test
+    public final void testOpenQuestion() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link domain.Question#closeQuestion()}.
+     */
+    @Test
+    public final void testCloseQuestion() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
